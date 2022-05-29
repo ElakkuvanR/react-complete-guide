9c6cd58015d63c639e18acb3dc521d2ea7c2f963
Elakkuvan Rajamani <elakkuvanbecs@gmail.com>
2022-05-30 00:36:13 +0200

    splitting the components into multiple

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    {
+      title: "Car Insurance",
+      amount: 300,
+      date: new Date(2022, 5, 28),
+    },
+    {
+      title: "Car Wash",
+      amount: 111,
+      date: new Date(2022, 3, 1),
+    },
+    {
+      title: "Friends Birthday Gift",
+      amount: 1500,
+      date: new Date(2022, 4, 12),
+    },
+  ];
   return (
     <div>
       <h2>Let's get started here!</h2>
-      <ExpenseItem></ExpenseItem>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      ></ExpenseItem>
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,15 @@
+import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
-  const expenseDate = new Date(2022, 5, 28);
-  const expenseTitle = 'Car Insurance';
-  const expenseAmount = 300;
-
+function ExpenseItem(props) {
+  
   return (
     <div className="expense-item">
-      <div>{expenseDate.toLocaleDateString()}</div>
+      <ExpenseDate date={props.date}></ExpenseDate>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
+        <h2>{props.title}</h2>
       </div>
-      <div className="expense-item__price">£{expenseAmount}</div>
+      <div className="expense-item__price">£{props.amount}</div>
     </div>
   );
 }
