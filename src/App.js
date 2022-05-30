1324af18044d31cf64affa2f2acd23b8da27fefb
Elakkuvan Rajamani <elakkuvanbecs@gmail.com>
2022-05-30 23:35:42 +0200

    Expense Form component is added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Expenses from './components/Expenses/Expenses';
+import NewExpense from './components/NewExpense/NewExpense';
 
-function App() {
+const App = () => {
   const expenses = [
     {
       title: "Car Insurance",
@@ -20,7 +21,7 @@ function App() {
   ];
   return (
     <div>
-      <h2>Let's get started here!</h2>
+      <NewExpense></NewExpense>
       <Expenses items={expenses} />
     </div>
   );
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,18 +1,25 @@
+import React, { useState } from "react";
 import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
-function ExpenseItem(props) {
-  
+const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title);
+
+  const clickHandlder = () => {
+    setTitle(title + " Updated");
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}></ExpenseDate>
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
       </div>
       <div className="expense-item__price">£{props.amount}</div>
+      <button onClick={clickHandlder}>Change Title</button>
     </Card>
   );
-}
+};
 
 export default ExpenseItem;
