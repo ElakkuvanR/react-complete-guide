300d746d6cf184b8e2258805ad6e4efb161f819f
Elakkuvan Rajamani <elakkuvanbecs@gmail.com>
2022-06-06 22:06:32 +0200

    Chart Component is added

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -43,7 +43,7 @@ const ExpenseForm = (props) => {
 
     const expenseDataObj = {
       title: enteredTitle,
-      amount: enteredAmount,
+      amount: +enteredAmount,
       date: new Date(enteredDate),
     };
     
